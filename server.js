0300acdf34fb99ba7d00e840c942cce4a38fb804
Cihad Horuzoglu <cihadhoruzoglu@gmail.com>
2014-05-04 18:26:45 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Setup
+var express 	= require('express'),				// get express module
+	app 		= express(),						// create our app w/ express
+	mongoose	= require('mongoose'),				// mongoose for mongodb
+	port 		= process.env.PORT || 8090,			// set the port
+	database	= 'mongodb://localhost/cicekapp';	// load the database
+
+// Configuration
+mongoose.connect(database);
+
+app.configure(function() {
+	app.use(express.static(__dirname + '/app')); 		    // set the static files location
+	app.use(express.logger('dev')); 						// log every request to the console
+	app.use(express.bodyParser()); 							// pull information from html in POST
+	app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+
+// Routes
+require('./routes.js')(app);
+
+// Listen (start app with 'node server.js') 
+app.listen(port);
+console.log("App listening on port " + port);
